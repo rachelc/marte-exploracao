c55e2210dba4d0e49c8c44e297da0f454aa943b8
RACHEL DE PAULA CAVALCANTI <rachel.cavalcanti@bp0743.grupo-buscape.com.br>
2015-06-29 16:13:32 -0300

    Json

--- src/main/java/com/elo7/marte/exploracao/controller/SondaController.java
+++ src/main/java/com/elo7/marte/exploracao/controller/SondaController.java
@@ -1,10 +1,9 @@
 package com.elo7.marte.exploracao.controller;
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.elo7.marte.exploracao.model.rest.SondaREST;
@@ -13,11 +12,15 @@ import com.elo7.marte.exploracao.model.rest.SondaREST;
 @RequestMapping("/sondas")
 public class SondaController {
 
-	@RequestMapping(value = "{sondaId}", method = GET)	
-	public @ResponseBody SondaREST getSonda (@PathVariable Integer sondaId){
+	@RequestMapping(value = "{sondaId}", method = RequestMethod.GET)	
+	public @ResponseBody 
+	SondaREST getSondaInJSON (@PathVariable Integer sondaId){
 		
 		
-		SondaREST sonda = new SondaREST(5, 1);
+		SondaREST sonda = new SondaREST();
+		sonda.setSondaId(sondaId);
+		sonda.setX(2);
+		sonda.setY(2);
 		
 		
 		return sonda;
